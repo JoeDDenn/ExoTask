b2e4ff90965c3f4b4e5a671469d1f61947d4ba64
Yousef Mohamed <ym79841@gmail.com>
2022-12-30 21:43:51 +0200

    cleanup

--- src/componants/Navbar/Nav.jsx
+++ src/componants/Navbar/Nav.jsx
@@ -6,26 +6,26 @@ const Nav = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg">
-  <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/">ExoTask</NavLink>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse collapseanim navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/login">Login</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/signup">Signup</NavLink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">ExoTask</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse collapseanim navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">Login</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/signup">Signup</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </nav>
       {/* <nav ClassName="navbar navbar-expand-lg bg-light">
         <div ClassName="container-fluid">
           <NavLink ClassName="navbar-brand" to="/">ExoTask</NavLink>
