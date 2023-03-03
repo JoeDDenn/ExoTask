a59dd442062b72883344b606ae9831842cc5483d
yousefsalemdev <salemyossif23@gmail.com>
2023-03-03 14:41:47 +0200

    edit

---
+++ src/componants/WorkSpace/bg/Bg.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Bg.css'
+const Bg = () => {
+  return (
+    <>
+        {/* <div className='bg-workspace'> </div> */}
+    
+      
+    </>
+  )
+}
+
+export default Bg
