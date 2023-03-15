423c6a4353360efbbcb52a9d8338345c8b0aa6a0
Yousef Mohamed <74785443+JoeDDenn@users.noreply.github.com>
2023-03-15 15:26:37 +0200

    add save blocks functionality

---
+++ src/componants/WorkSpace/AddBlock.js
@@ -0,0 +1,66 @@
+
+import { v4 as uuid } from 'uuid';
+
+function AddBlock(type, block, className) {
+
+    //find workspace
+    const workspace = document.getElementById('workspace');
+
+    
+    //create new block
+    const newBlock = document.createElement('div');
+    newBlock.className = "Block";
+    const id = newBlock.id = uuid();
+    switch (type) {
+        case 'paragraph':
+            //add a paragraph to new block
+            const newParagraph = document.createElement('p');
+            newParagraph.innerHTML = block.data.text;
+            newParagraph.contentEditable = true;
+            newParagraph.className = className;
+            newBlock.appendChild(newParagraph);
+            break;
+        case 'heading':
+            const newHeading = document.createElement('h1');
+            newHeading.innerHTML = block.data.text;
+            newHeading.contentEditable = true;
+            newHeading.className = className;
+            newBlock.appendChild(newHeading);
+            break;
+        case 'image':
+            //add a image to new block
+            const newImage = document.createElement('img');
+            newImage.src = block.data.file.url;
+            newImage.className = className;
+            newBlock.appendChild(newImage);
+            break;
+        case 'webpage':
+            const newIframe = document.createElement('iframe');
+            newIframe.className = className;
+            // ask user for url
+            const url = block.data.url;
+            //set src of iframe to url
+            newIframe.src = url;
+            newBlock.appendChild(newIframe);
+            break;
+        default:
+            break;
+    }
+    //add new block to workspace
+    workspace.appendChild(newBlock);
+    //set focus on new block
+    //add a delete button to new block with id
+    const newDeleteButton = document.createElement('button');
+    newDeleteButton.className = "del-but";
+    //add child Icon to button
+    const newDeleteIcon = document.createElement('i');
+    newDeleteIcon.className = "fa fa-trash";
+    newDeleteButton.appendChild(newDeleteIcon);
+    newDeleteButton.onclick = () => {
+        const block = document.getElementById(id);
+        block.parentNode.removeChild(block);
+    }
+    newBlock.appendChild(newDeleteButton);
+}
+export default AddBlock;
+
--- src/componants/WorkSpace/HomeWorkSpace.jsx
+++ src/componants/WorkSpace/HomeWorkSpace.jsx
@@ -6,37 +6,136 @@ import AnimatedBackground from './bg/AnimatedBackground '
 import Chatbot from '../Chat/ChatBot'
 import AddComp from '../AddComp/AddComp'
 import KanBanBoard from '../KanBanBoard/KanBanBoard'
-import Popup from '../Popup/Popup'
+import AddBlock from './AddBlock'
+import { useEffect } from 'react'
 
 
 
+const board = {
+  id: 'board-1',
+  title: 'My board',
+  taskLists : [
+    {
+      id: 'taskList-1',
+      title: 'To do',
+      tasks: [
+        {
+          id: 'task-1',
+          title: 'Learn React',
+          description: 'Learn how to use React to build web applications',
+          status: 'todo'
+        },
+        {
+          id: 'task-2',
+          title: 'Learn Redux',
+          description: 'Learn how to use Redux to manage the state of web applications',
+          status: 'todo'
+        }
+      ]
+    },
+    {
+      id: 'taskList-2',
+      title: 'In progress',
+      tasks: [
+        {
+          id: 'task-3',
+          title: 'Learn Webpack',
+          description: 'Learn how to use Webpack to bundle web applications',
+          status: 'in-progress'
+        }
+      ]
+    },
+    {
+      id: 'taskList-3',
+      title: 'Done',
+      tasks: [
+        {
+          id: 'task-4',
+          title: 'Learn Node.js',
+          description: 'Learn how to use Node.js to build web servers',
+          status: 'done'
+        }
+      ]
+    }
+  ]
+}
 
+const Blocks = {
+  wsID : '1',
+  Blocks : [
+    {
+      id: 'block-1',
+      type: 'paragraph',
+      classname : 'WSParagraph',
+      data: {
+        text: 'Hey. This is a simple text block'
+      }
+    },
+    {
+      id: 'block-2',
+      type: 'heading',
+      classname : 'WSHeading',
+      data: {
+        text: 'This is a header',
+        level: 2
+      }
+    },
+    {
+      id: 'block-3',
+      type: 'image',
+      classname : 'image',
+      data: {
+        file: {
+          url: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'
+        },
+        caption: 'This is an image',
+      }
+    },
+    {
+      id: 'block-4',
+      type: 'webpage',
+      classname : 'WSFrame',
+      data: {
+        url: 'https://www.youtube.com/embed/Vqa9NMzF3wc',
+      }
+    },
+  ]
+}
+
+
+const buildBlocks = (blocklist) => {
+  for (let i = 0; i < blocklist.length; i++) {
+    AddBlock(blocklist[i].type, blocklist[i], blocklist[i].classname)
+  }
+}
 
 const HomeWorkSpace = () => {
+  useEffect(() => {
+    buildBlocks(Blocks.Blocks)
+  }, []);
+
   if(localStorage.getItem('token') === null) window.location.replace('/login')
   else{
-  return (
-    <div className='wrapper'>
-    <AnimatedBackground>
-     <NavWorkSpcae/>
-      <SideBar/>
-     {/* <Main/>  */}
-    
-    <div className='container'>
-    <AddComp/>
-     <div className='workspace container' id='workspace'>
-        <div className='Block' id="1">
-          <KanBanBoard/>
-        </div>
-     </div>
-    </div>
-
-     <Chatbot/>
-    </AnimatedBackground>
-    </div>
-
-  )
+    return (
+      <div className='wrapper'>
+      <AnimatedBackground>
+       <NavWorkSpcae/>
+        <SideBar/>
+       {/* <Main/>  */}
+      <div className='container'>
+      <AddComp/>
+       <div className='workspace container' id='workspace'>
+          <div className='Block' id="1">
+            <KanBanBoard state={board} />
+          </div>
+       </div>
+      </div>
+       <Chatbot/>
+      </AnimatedBackground>
+      </div>
+    )
+  }
 }
-}
+
 
 export default HomeWorkSpace
